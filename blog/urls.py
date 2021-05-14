5b264b036774697a84e2fac43faa6db650b4d3da
paulina <p.tymoszuk@op.pl>
2021-05-14 17:23:20 +0200

    second commit

--- blog/urls.py
+++ blog/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from polls.views import home_view, base_page_view, create_post_view, create_user_view, posts_list_view, post_lookup_view
+from polls.views import home_view, base_page_view, create_post_view, create_user_view, posts_list_view, post_lookup_view, user_table_viev
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -26,7 +26,7 @@ urlpatterns = [
     path('post', create_post_view),
     path('posts/', posts_list_view),
     path('posts/<int:id>', post_lookup_view, name='post-detail'),
-    path('user/<int:id>', post_lookup_view, name='user'),
+    path('user/<int:id>', user_table_viev, name='user-tab'),
     path('adduser', create_user_view),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
